61e10801e496dada7d8f63511ce7574d4e9de293
SAC-CS112-Diego-Castillo <diego-castillo@nova-student.org>
2017-05-14 17:08:34 -0700

    Add files via upload

---
+++ dice.java
@@ -0,0 +1,32 @@
+
+public class Dice 
+
+{
+
+	public double Throw()
+
+	{
+
+		int dice;
+
+		dice = (int)(Math.random() * 6) + 1;
+
+		return dice;
+
+	}
+
+	
+
+		
+
+	@Override
+
+	public String toString()
+
+	{
+
+		return String.format("The current value is: %d", Throw());
+
+	}
+
+} // end class Dice
